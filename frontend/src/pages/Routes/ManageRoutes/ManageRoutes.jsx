75f2f0124fd52abef12e0a9421dee0a3d1ca52f2
Yousef Mahmoud <mryoyo133@gmail.com>
2026-01-25 12:23:27 +0300

    add give admin the ability to delete routes

--- frontend/src/pages/Routes/ManageRoutes/ManageRoutes.jsx
+++ frontend/src/pages/Routes/ManageRoutes/ManageRoutes.jsx
@@ -5,44 +5,84 @@ import { useNavigate, useOutletContext } from "react-router";
 import RouteItem from "../../../components/Routes/RouteItem";
 import Stars from "../../../components/Routes/Stars";
 import Loading from "../../../components/Loading.jsx";
+import { deleteRouteRequest } from "../../../api/routes";
 
-export default function ManageRoutes(){
-    const [routes, setRoutes] = useState([]);
-    const {type} = useOutletContext();
-    const navigate = useNavigate();
-    const popup = usePopup();
-    const [loading, setLoading] = useState(true);
+export default function ManageRoutes() {
+  const [routes, setRoutes] = useState([]);
+  const { type } = useOutletContext();
+  const navigate = useNavigate();
+  const popup = usePopup();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        const fetchData = async ()=>{
-            setLoading(true);
-            let res = await getAllRoutes(type);
-            if(res.success){
-                setRoutes(res.data)
-            }
-            else{
-                popup.showError("Request failed: " + res.error);
-            }
-            setLoading(false);
-        }
-        fetchData()
-    }, [type])
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      let res = await getAllRoutes(type);
+      if (res.success) {
+        setRoutes(res.data);
+      } else {
+        popup.showError("Request failed: " + res.error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [type]);
 
-    const handelManage = (e) =>{
-        let id = e.target.closest('[id*="route"]').id.replace("route", "")
-        navigate(`/manageroute/${id}`)
-    }
+  const handelManage = (e) => {
+    let id = e.target.closest('[id*="route"]').id.replace("route", "");
+    navigate(`/manageroute/${id}`);
+  };
 
+  const handelDelete = (e) => {
+    const deleteRequest = async () => {
+      let confirm = await popup.showConfirm(
+        "Are you sure you want to delete this route?",
+      );
+      if (!confirm) return;
 
-    return (
-        <>
-            {loading ? <Loading/> :
-            routes.length === 0 ? <span className="text-text-muted text-xl text-center mt-8">No routes available.</span> :
-            routes.map((r)=>{
-                return <RouteItem key={r.id} route={r} handelClick={handelManage}>
-                    <Stars route={r}/>
-                </RouteItem>
-            })}
-        </>
-    )
+      let id = e.target.closest('[id*="route"]').id.replace("route", "");
+      setLoading(true);
+      let res = await deleteRouteRequest(id);
+      if (res.success) {
+        let newRoutes = routes.filter((r) => r.id !== id);
+        popup.showSuccess("Route deleted successfully.");
+        setRoutes(newRoutes);
+      } else {
+        popup.showError("Delete request failed: " + res.error);
+      }
+      setLoading(false);
+    };
+    deleteRequest();
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : routes.length === 0 ? (
+        <span className="text-text-muted text-xl text-center mt-8">
+          No routes available.
+        </span>
+      ) : (
+        routes.map((r) => {
+          return (
+            <RouteItem key={r.id} route={r} handelClick={handelManage}>
+              <button
+                className="flex items-center justify-center"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handelDelete(e);
+                }}
+              >
+                <span className="material-symbols-rounded w-12  bg-danger rounded-2xl">
+                  close
+                </span>
+              </button>
+              <Stars route={r} />
+            </RouteItem>
+          );
+        })
+      )}
+    </>
+  );
 }
